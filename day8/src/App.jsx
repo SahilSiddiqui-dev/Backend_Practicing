f6e2c92681eafd775453ce56a3d9698073d6bf4f
Mohd Sahil <sahilsiddiqui2424@gmail.com>
2026-02-13 19:15:47 +0530

    new file:   day8/src/App.jsx

---
+++ day8/src/App.jsx
@@ -0,0 +1,15 @@
+import Event from './components/EventToDo.jsx'
+import Count from './components/Counter.jsx'
+import './components/EventToDo.css'
+import './App.css'
+function App() {
+
+  return (
+    <div className='div0'>
+     <Event/>
+     <Count/>
+    </div>
+  )
+}
+
+export default App
