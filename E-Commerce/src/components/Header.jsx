cdc432c3b4628f3a9efd4b4c8e74c4ab59f86a80
Mohd Sahil <sahilsiddiqui2424@gmail.com>
2026-02-21 22:57:29 +0530

    adding all the files to the staging area and commiting with the message "initial commit"

---
+++ E-Commerce/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Headers({cartCount}){
+    return (
+        <div>
+            <h2>React Demo App</h2>
+            <p>Cart Item: {cartCount} </p>
+        </div>
+    );
+}
