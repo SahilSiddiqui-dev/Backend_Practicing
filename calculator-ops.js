6eb24f069704bcfa2da94643b9dafe62b43c7d29
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-08-12 11:45:12 +0530

    add calculator operations and main file for basic arithmetic functions

---
+++ calculator-ops.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+
+  return a / b;
+}
+
+function calculate(a, operator, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      throw new Error('Invalid operator');
+  }
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  calculate,
+};
