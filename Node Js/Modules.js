553c6ce7617b0e34701dfae9e3ca3f01fc486a40
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-08-17 12:29:14 +0530

    Add Modules.js and tempCodeRunnerFile.js with math operations

---
+++ Node Js/Modules.js
@@ -0,0 +1,7 @@
+const os = require('os');
+const math = require("./math.js");
+console.log(os.type());
+console.log(math.add(5,10));
+console.log(math.substract(5,10));
+console.log(math.multiply(5,10));
+console.log(math.divide(5,10));
