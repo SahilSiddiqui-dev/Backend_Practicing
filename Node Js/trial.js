d348dba6cfb67104209acb79caf3f198710e4100
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-09-07 11:10:23 +0530

    feat: add error handling middleware and improve teacher router integration

--- Node Js/trial.js
+++ Node Js/trial.js
@@ -5,16 +5,22 @@ const teacherRouter = require('./teacher')
 app.use(express.json());
 
 app.use((req, res, next) => {
-     app.use('/teachers', teacherRouter);
+    app.use('/teachers', teacherRouter, (req, res, next));
     console.log(`${req.method} ${req.url}`);
-   
     next();
 })
 
+app.get("/", (req, res, next) => {
+    try {
+        res.end("Hello you are at Home Page");
+    } catch (err) {
+        next(err);
+    }
+})
 
-
-app.get("/", (req, res) => {
-    res.end("Hello you are at Home Page");
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send("Something went wrong");
 })
 
 app.listen(3000, () => {
