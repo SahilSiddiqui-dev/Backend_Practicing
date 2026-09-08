4ebb08c2b8942a9b4a3c18d670c989c063dd161c
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-09-09 01:52:13 +0530

    feat: add tests for circular references, elements, and fakes in object-inspect module

--- Node Js/trial.js
+++ Node Js/trial.js
@@ -2,26 +2,56 @@ const express = require('express');
 const app = express();
 const teacherRouter = require('./teacher')
 
+
 app.use(express.json());
 
 app.use((req, res, next) => {
-    app.use('/teachers', teacherRouter, (req, res, next));
-    console.log(`${req.method} ${req.url}`);
+    console.log("middleware 1");
+    console.log(req.method, req.url);
     next();
 })
 
-app.get("/", (req, res, next) => {
-    try {
-        res.end("Hello you are at Home Page");
-    } catch (err) {
-        next(err);
-    }
+app.use((req, res, next) => {
+    console.log("middleware 2");
+    next();
 })
 
-app.use((err, req, res, next) => {
-    console.log(err.stack);
-    res.status(500).send("Something went wrong");
+app.use("/teachers", teacherRouter, (req, res, next) => {
+    console.log("Requested URL", req.url);
+    next();
+});
+
+app.use("/teachers/:id", (req, res, next) => {
+    if(req.params.id === '0') {
+        next('route');
+    } 
+    else {
+        next()
+    }
+
+    },
+(req, res) => {
+    res.send("Regular Route")
+}
+);
+
+app.get("/teachers/:id",  teacherRouter ,(req, res, next) => {
+    res.send('Special route')
 })
+
+
+// app.get("/", (req, res, next) => {
+//     try {
+//         res.end("Hello you are at Home Page");
+//     } catch (err) {
+//         next(err);
+//     }
+// })
+
+// app.use((err, req, res, next) => {
+//     console.log(err.stack);
+//     res.status(500).send("Something went wrong");
+// })
 
 app.listen(3000, () => {
     console.log("server running on http://localhost:3000");
