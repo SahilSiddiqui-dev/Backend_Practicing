7672ebc5cf3f43ec693f138949283311cf86020c
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-09-11 00:58:29 +0530

    Add qs library and update server routes
    - Added the qs library with its LICENSE and README files.
    - Created ESLint configuration for the qs library.
    - Added package.json for the qs library with necessary dependencies and scripts.
    - Updated server.js to include logging middleware and corrected teacher router variable name.

--- Node Js/server.js
+++ Node Js/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const studentRoutes = require('./post.js');
-const teacherRouter  = require('./teacher.js');
+const teacherRoutes = require('./teacher.js');
+const checkRole = require('./role_middleware');
 const app = express();
 const port = 3000;
 
+
+app.use((req, res, next) => {
+  console.log("Request coming from ", req.originalUrl);
+  console.log("Request method is ", req.method);
+  next();
+});
+
 app.use(express.json());
 app.use('/students', studentRoutes);
-app.use('/teachers', teacherRouter);
+app.use('/teachers', teacherRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
